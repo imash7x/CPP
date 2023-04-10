ee81fa7ebea1600f52f88e5bb80e9cf19912313e
imash7x <ashishp2d@gmail.com>
2023-04-10 10:54:29 +0530

    array

--- array/insertinsortedarray.cpp
+++ array/insertinsortedarray.cpp
@@ -1,34 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int findElement(int arr[],int first, int last, int key){
-    int mid = (first+last)/2;
-    if(last >= first){
-        if(arr[mid] == key){
-            return mid;
-        }
-        else if(key > arr[mid]){
-            return findElement(arr,mid+1,last,key);
-        }
-        else{
-        return findElement(arr,first,mid-1,key);
-        }
-    }return -1;
-}
-int deleteElement(int arr[], int n, int key){
-    int pos = findElement(arr, 0, n-1, key);
-    
-    if(pos == -1){
-        cout<<"element not found.";
+int insertElement(int arr[], int n,int capacity,int key){    
+    if(n>=capacity){
+        cout<<"cant insert array full!";
         return n;
     }
-    for(int i=pos; i<n-1; i++){
-        arr[i]=arr[i+1];
-    }return n-1;
+    int i;
+    for(i=n-1; (arr[i]>=key && i>=0); i--)
+        arr[i+1]=arr[i];   
+    
+    arr[i+1]=key;
+    return(n+1);
 }
 int main(){
-    int arr[] = {2,3,4,5,6,7};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[8] = {2,3,5,6,7};
+    int capacity = sizeof(arr)/sizeof(arr[0]);
+    int n = 5;
     int key;
     cout<<"enter element: ";
     cin>>key;
@@ -38,7 +26,7 @@ int main(){
         cout<<arr[i]<<", ";
     }
     
-    deleteElement(arr,n,key);
+    insertElement(arr,n,capacity,key);
     
     cout<<"\nAfter: ";
     for(int i=0; i<n; i++){
